3e52231d764906f527da9e2e6f10574da052afd4
yuinakorn <yuinakorn@hotmail.com>
2024-03-01 13:05:32 +0700

    fix bug: table_name

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ table_name = "dustboy_value"
 def import_to_133(df):
     try:
         with engine133.connect() as con133:
-            df.to_sql('irf_backup', con133, if_exists='replace', index=False)
+            df.to_sql(table_name, con133, if_exists='replace', index=False)
             print(df)
             count_rows = df.shape[0]
             print(f"Data inserted {count_rows} rows to database 133 successfully!")
